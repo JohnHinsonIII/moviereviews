981d24d8b8849c0afda384a62d98820f1846614d
JohnHinson-III <admin@gmail.com>
2022-09-02 12:54:02 -0400

    second version

--- movie/models.py
+++ movie/models.py
@@ -7,6 +7,9 @@ class Movie(models.Model):
     image = models.ImageField(upload_to='movie/images/')
     url = models.URLField(blank=True)
     
+    def __str__(self):
+        return self.title
+    
 class Review(models.Model):    
     text = models.CharField(max_length=100)    
     date = models.DateTimeField(auto_now_add=True)
